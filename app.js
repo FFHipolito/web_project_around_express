3b1aefacfe900d114887b2fd19ce480d09a9be69
FernandoHipolito <fernandofhipolito@gmail.com>
2024-06-25 09:17:18 -0300

    fix: solving lint problems and remove extra code

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const PORT = 3000;
-const cardsRouter = require("./routes/cards");
-const usersRouter = require("./routes/users");
+const cardsRouter = require('./routes/cards');
+const usersRouter = require('./routes/users');
 
-app.use("/cards", cardsRouter);
-app.use("/users", usersRouter);
-
-app.get("/", (req, res) => {
-  res.send("I am Developer!");
-});
+app.use('/cards', cardsRouter);
+app.use('/users', usersRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "A solicitação não foi encontrada" });
+  res.status(404).json({ message: 'A solicitação não foi encontrada' });
 });
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   fs.readFile(
-    path.join(__dirname, "..", "data", "users.json"),
-    "utf8",
+    path.join(__dirname, '..', 'data', 'users.json'),
+    'utf8',
     (err, data) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ message: "Erro interno do servidor" });
+        res.status(500).json({ message: 'Erro interno do servidor' });
         return;
       }
 
       const users = JSON.parse(data);
       res.json(users);
-    }
+    },
   );
 });
 
-router.get("/:userId", (req, res) => {
+router.get('/:userId', (req, res) => {
   const { userId } = req.params;
 
   fs.readFile(
-    path.join(__dirname, "..", "data", "users.json"),
-    "utf8",
+    path.join(__dirname, '..', 'data', 'users.json'),
+    'utf8',
     (err, data) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ message: "Erro interno do servidor" });
+        res.status(500).json({ message: 'Erro interno do servidor' });
         return;
       }
 
@@ -38,12 +38,12 @@ router.get("/:userId", (req, res) => {
       const user = users.find((user) => user._id === userId);
 
       if (!user) {
-        res.status(404).json({ message: "ID do usuário não encontrado" });
+        res.status(404).json({ message: 'ID do usuário não encontrado' });
         return;
       }
 
       res.json(user);
-    }
+    },
   );
 });
 
