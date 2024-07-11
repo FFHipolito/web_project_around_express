c7f93721c52be74fdb99dce63fd7478430c8f695
FernandoHipolito <fernandofhipolito@gmail.com>
2024-07-11 13:13:26 -0300

    installed MongoDB

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cardsRouter = require("./routes/cards");
+const usersRouter = require("./routes/users");
 
 const app = express();
-const PORT = 3000;
-const cardsRouter = require('./routes/cards');
-const usersRouter = require('./routes/users');
+const { PORT = 3000 } = process.env;
 
-app.use('/cards', cardsRouter);
-app.use('/users', usersRouter);
+app.use("/cards", cardsRouter);
+app.use("/users", usersRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'A solicitação não foi encontrada' });
+  res.status(404).json({ message: "A solicitação não foi encontrada" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+mongoose
+  .connect("mongodb://localhost:27017/aroundb")
+  .then(() => {
+    console.log(`MongoDB connected...`);
+
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
